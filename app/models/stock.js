806dd22414282bc1d95955f9ca41982f1550c9a7
Eliot Glairon <ejg_dnd@yahoo.com>
2017-02-16 18:26:58 +0000

    Removed passport, initial setup

---
+++ app/models/stock.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Stock = new Schema({
+   symbol: String 
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,32 +3,22 @@
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app, passport) {
+module.exports = function (app) {
 
 	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
+		return next();
 	}
 
 	var clickHandler = new ClickHandler();
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
+	/*app.route('/login')
 		.get(function (req, res) {
 			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
 		});
 
 	app.route('/profile')
@@ -53,5 +43,5 @@ module.exports = function (app, passport) {
 	app.route('/api/:id/clicks')
 		.get(isLoggedIn, clickHandler.getClicks)
 		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+		.delete(isLoggedIn, clickHandler.resetClicks);*/
 };
